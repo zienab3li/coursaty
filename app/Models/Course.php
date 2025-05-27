c2f0f2ce861a0e502abcd9fbd363fccf0f7417d9
zienab3li <zienab2152002@gmail.com>
2025-05-27 22:48:19 +0300

    models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,9 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     /** @use HasFactory<\Database\Factories\CourseFactory> */
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'instructor_id'
+
+    ];
+     public function instructor(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'instructor_id');
+    }
+
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'course_student')->withTimestamps();
+    }
+
+    public function sessions(): HasMany
+    {
+        return $this->hasMany(CourseSession::class);
+    }
+
+    
 }
--- app/Models/CourseSession.php
+++ app/Models/CourseSession.php
@@ -4,9 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CourseSession extends Model
 {
     /** @use HasFactory<\Database\Factories\CourseSessionFactory> */
     use HasFactory;
+    protected $fillable = [
+         'title',
+        'content',
+        'course_id',
+    ];
+     public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
